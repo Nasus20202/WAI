b06ef4334f50b16295e0c519efaf20b0b70254c3
Krzysztof Nasuta <krzysztof.nasuta@wp.pl>
2022-10-13 18:57:45 +0200

    Navbar works

--- src/layouts/main.php
+++ src/layouts/main.php
@@ -14,14 +14,21 @@
     <title><?php echo $title ?></title>
     <link rel="stylesheet" href="<?php echo $basePath?>/assets/css/style.css">
     <link rel="stylesheet" href="<?php echo $basePath?>/assets/css/responsive.css">
+    <script src="<?php echo $basePath?>/assets/js/main.js"></script>
   </head>
   <body>
+    <header>
+      <div class="center">
+        <img src="<?php echo $basePath?>/assets/img/logo.png" class="image logo" alt="keebee logo">
+        <span class="title">Keebee</span>
+      </div>
+    </header>
     <div class="container">
-      <nav>
+      <nav id="sticky-navbar">
         <?php
         foreach (array_keys($routes) as &$routeName){
           if($routes[$routeName][2]){
-            echo '<a href="'. $routeName .'" class="navitem">' . $routes[$routeName][1] . '</a>';
+            echo '<a href="'. $routeName .'" class="navitem' . ($path == $routeName ? ' currentNav' : '') . '">' . $routes[$routeName][1] . '</a>';
           }
         }?>
               </nav>
