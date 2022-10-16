5023075e9ad14813e65604e47b8d1302d6403ce9
Krzysztof Nasuta <krzysztof.nasuta@wp.pl>
2022-10-16 22:44:53 +0200

    validation fix

--- src/layouts/main.php
+++ src/layouts/main.php
@@ -1,26 +1,23 @@
 <!DOCTYPE html>
 <html lang="pl">
   <head>
-    <meta charset="utf-8" />
-    <link rel="icon" href="<?php echo $basePath?>/assets/img/favicon.ico" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="theme-color" content="#F5CB5C" />
-    <meta name="keywords" content="keebee, klawiatury" />
-    <meta name="author" content="Krzysztof Nasuta" />
-    <meta
-      name="description"
-      content="Keebee"
-    />
+    <meta charset="utf-8">
+    <link rel="icon" href="<?php echo $basePath?>/assets/img/favicon.ico">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="theme-color" content="#F5CB5C">
+    <meta name="keywords" content="keebee, klawiatury">
+    <meta name="author" content="Krzysztof Nasuta">
+    <meta name="description" content="Keebee">
     <title><?php echo $title ?></title>
-    <link rel="stylesheet" href="<?php echo $basePath?>/assets/css/style.css" />
-    <link rel="stylesheet" href="<?php echo $basePath?>/assets/css/responsive.css" />
+    <link rel="stylesheet" href="<?php echo $basePath?>/assets/css/style.css">
+    <link rel="stylesheet" href="<?php echo $basePath?>/assets/css/responsive.css">
     <script src="<?php echo $basePath?>/assets/js/main.js"></script>
   </head>
   <body>
     <div id="theme-toggler" onclick="toggleTheme()"></div>
     <header>
       <div class="center">
-        <img src="<?php echo $basePath?>/assets/img/logo.png" class="image logo" alt="keebee logo" />
+        <img src="<?php echo $basePath?>/assets/img/logo.png" class="image logo" alt="keebee logo">
         <span class="title">Keebee</span>
       </div>
     </header>
