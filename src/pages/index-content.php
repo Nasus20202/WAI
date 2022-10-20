32f1164b9f5f6a8175502a5a1a20b980fef8389c
Krzysztof Nasuta <krzysztof.nasuta@wp.pl>
2022-10-20 22:32:11 +0200

    Index page

--- src/pages/index-content.php
+++ src/pages/index-content.php
@@ -8,7 +8,10 @@
 
 <article class="card card-right">
     <div class="card-content">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi iusto tempora libero at, officia molestiae corrupti, asperiores accusantium nesciunt in aliquid! Quae vel, distinctio praesentium quod nam maiores expedita culpa!
+        Klawiatury dzielimy na membranowa i mechaniczne. Pierwsze z nich są dużo tańsze. Niestety, wiąże się to często z ograniczoną żywotnością oraz niskim komfortem użytkowania.
+        Z tego powodu bardzo popularne są klawiatury mechaniczne. Zamiast zwykłej membrany, która jest bardzo podatna na zużycie oraz nie charakteryzuje się przyjemnym klikiem,
+        stosuje się w nich specjalne przełączniki. Są one w pełni mechaniczne. Naciśnięcie klawisza powoduje zamknięcie obwodu elektrycznego. Informacja o tym jest następnie
+        przekazywana do komputera. Dzięki takiej konstrukcji przełącznik jest dużo bardziej wytrzymały, a użytkowanie klawiatury staje się czystą przyjemnością.
     </div>
     <img class="card-img" src="<?php echo $basePath?>/assets/img/kb0-min.jpg" alt="Klawiatura">
 </article>
@@ -16,17 +19,18 @@
 <article class="card card-left">
     <img class="card-img" src="<?php echo $basePath?>/assets/img/kb4-min.jpg" alt="Klawiatura">
     <div class="card-content">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi iusto tempora libero at, officia molestiae corrupti, asperiores accusantium nesciunt in aliquid! Quae vel, distinctio praesentium quod nam maiores expedita culpa!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi iusto tempora libero at, officia molestiae corrupti, asperiores accusantium nesciunt in aliquid! Quae vel, distinctio praesentium quod nam maiores expedita culpa!
+        Główną zaletą klawiatury mechanicznej jest komfort użytkowania. Różnica między nią a klawiaturą membranową jest diametralna. Ważnym aspektem jest także duża możliwość
+        dostosowania klawiatury do własnych potrzeb. Możliwe jest zbudowanie własnej klawiatury wykorzystując jedynie odpowiednie podzespoły. Pozwala to na uzyskanie klawiatury
+        idealnie dopasowanej do użytkownika. Klawiatury mechaniczne cechują się także wysoką wytrzymałością (do 100 mln kliknięć na przełącznik). Jedyną wadą jest ich cena.
     </div>
 </article>
 
-<article class="card">
+<article class="card card-right">
     <div class="card-content">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi iusto tempora libero at, officia molestiae corrupti, asperiores accusantium nesciunt in aliquid! Quae vel, distinctio praesentium quod nam maiores expedita culpa!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi iusto tempora libero at, officia molestiae corrupti, asperiores accusantium nesciunt in aliquid! Quae vel, distinctio praesentium quod nam maiores expedita culpa!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi iusto tempora libero at, officia molestiae corrupti, asperiores accusantium nesciunt in aliquid! Quae vel, distinctio praesentium quod nam maiores expedita culpa!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi iusto tempora libero at, officia molestiae corrupti, asperiores accusantium nesciunt in aliquid! Quae vel, distinctio praesentium quod nam maiores expedita culpa!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi iusto tempora libero at, officia molestiae corrupti, asperiores accusantium nesciunt in aliquid! Quae vel, distinctio praesentium quod nam maiores expedita culpa!
+        Klawiatury mechaniczne można w bardzo łatwy sposób modyfikować. Społeczność tworząca się wokół nich jest bardzo aktywna. Wystarczy odwiedzić odpowiednie fora, aby
+        dowiedzieć się wielu ciekawych informacji. Można na nich znaleźć wiele ciekawych pomysłów na modyfikacje klawiatury. Wiele osób tworzy własne klawiatury, które
+        są w pełni dopasowane do swoich potrzeb. Często wykorzystują oni ciekawe techniki, takie jak naklejenie taśmy malarskiej na dół PCB. Pozwala to na uzyskanie
+        lepszego i głębszego dźwięku. Wiele osób również umieszcza w przełącznikach specjalny smar, który sprawia, że przełączniki są bardziej gładkie.
     </div>
+    <img class="card-img" src="<?php echo $basePath?>/assets/img/kb7-min.jpg" alt="Klawiatura">
 </article>
