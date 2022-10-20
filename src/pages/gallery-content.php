acd4ef0837be6bb3e4ea500c707c6831d14435bc
Krzysztof Nasuta <krzysztof.nasuta@wp.pl>
2022-10-20 16:40:09 +0200

    SVG

--- src/pages/gallery-content.php
+++ src/pages/gallery-content.php
@@ -1,8 +1,19 @@
 <script src="<?php echo $basePath ?>/assets/js/gallery.js"></script>
 <link rel="stylesheet" href="<?php echo $basePath ?>/assets/css/gallery.css">
 <section>
-    <h3>Galeria</h3>
     <section>
+        <svg id="logo" height="10vh" width="100%">
+            <defs>
+                <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%">
+                <stop offset="0%" style="stop-color:rgb(36,36,35);stop-opacity:1" />
+                <stop offset="60%" style="stop-color:rgb(219,182,79);stop-opacity:2" />
+                <stop offset="100%" style="stop-color:rgb(245,203,92);stop-opacity:2" />
+                </linearGradient>
+            </defs>
+            <rect  x="0" y="0" rx="20" ry="20" width="100%" height="100%"  fill="url(#grad1)" />
+            <text fill="#ebc65f" font-size="7vh" font-family="Lato" x="3%" y="75%">Galeria</text>
+            Galeria
+        </svg>
         <noscript class="gallery">
             <a href="<?php echo $basePath ?>/assets/img/kb1.jpg">
                 <img class="gallery-photo" src="<?php echo $basePath?>/assets/img/kb0-min.jpg" alt="Klawiatura">
