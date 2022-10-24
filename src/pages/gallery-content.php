36219e1b57bef9cc3d9ed9d0bec78a49e19d80b1
Krzysztof Nasuta <krzysztof.nasuta@wp.pl>
2022-10-24 19:27:07 +0200

    404 error fix

--- src/pages/gallery-content.php
+++ src/pages/gallery-content.php
@@ -59,6 +59,6 @@
         </div>
         <div id="modal" class="modal">
             <span class="close" onclick="closeModal()">&times;</span>
-            <img class="modal-content" id="modal-img" src="<?php echo $basePath?>/assets/img/logo.jpg" alt="modal">
+            <img class="modal-content" id="modal-img" src="<?php echo $basePath?>/assets/img/logo.png" alt="modal">
         </div>
 </div>
